8096101fbc3946eff4d1d36305db4725203c1177
Qu <Quanvle@live.dk>
2026-07-29 01:23:50 +0200

    Storing form data in the database
    Create a entity

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    username = models.CharField(max_length = 500,unique = True, error_messages= {
+        'unique' : "A user with that username already exists.",
+        'required' : "This field is required.",
+        'blank' : "This field may not be blank.",
+        'null' : "This field may not be null.",
+    })
+    review_text = models.TextField()
+    rating = models.IntegerField()
+    review_date = models.DateField(auto_now_add=True)
