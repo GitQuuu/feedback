1f73a55c5726bae62eee7b5e04b99043cea3385b
Qu <Quanvle@live.dk>
2026-07-28 13:36:25 +0200

    Configure project environment and fix review form

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedicrect
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def review(request):
     if request.method == 'POST':
         username = request.POST['username']
         print(username)
-    return HttpResponseRedicrect('/thank-you')
+        return HttpResponseRedirect('/thank-you')
+    return render(request, 'reviews/review.html')
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
