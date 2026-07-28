50b7349926f1e704b7a16f38c217bb0192d554a2
Qu <Quanvle@live.dk>
2026-07-29 01:48:17 +0200

    explicit use ModelForm to map the fields as form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,33 +1,40 @@
 from django import forms
 
+from reviews.models import Review
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(
-        min_length=2,
-        max_length=15,
-        label="Your username",
-        error_messages={
-            'required': 'Please enter a username.',
-            'min_length': 'Please enter a username greater than 2.',
-        },
-    )
-    review_text = forms.CharField(
-        max_length=500,
-        widget=forms.Textarea,
-        label="Your review",
-        error_messages={
-            'required': 'Please enter a review.',
-            'max_length': 'Please enter a review.',
-        }
-    )
-    rating = forms.IntegerField(
-        widget=forms.NumberInput(
-            attrs={
-                'class': 'form-control',
-            }
-        ),
-        error_messages={
-            'required': 'Please enter a review.',
-        }
-    )
+
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(
+#         min_length=2,
+#         max_length=15,
+#         label="Your username",
+#         error_messages={
+#             'required': 'Please enter a username.',
+#             'min_length': 'Please enter a username greater than 2.',
+#         },
+#     )
+#     review_text = forms.CharField(
+#         max_length=500,
+#         widget=forms.Textarea,
+#         label="Your review",
+#         error_messages={
+#             'required': 'Please enter a review.',
+#             'max_length': 'Please enter a review.',
+#         }
+#     )
+#     rating = forms.IntegerField(
+#         widget=forms.NumberInput(
+#             attrs={
+#                 'class': 'form-control',
+#             }
+#         ),
+#         error_messages={
+#             'required': 'Please enter a review.',
+#         }
+#     )
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['username','review_text', 'review_rating', 'review_date']
 
