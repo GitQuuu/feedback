9fc23cd8bcfd3ee31e6500604ca625b81bb9a5f6
Qu <Quanvle@live.dk>
2026-07-28 17:26:45 +0200

    Custom form validation use novalidate during development on form

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,5 +2,13 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    username = forms.CharField(min_length=2, max_length=15)
+    username = forms.CharField(
+        min_length=2,
+        max_length=15,
+        label="Your username",
+        error_messages={
+            'required': 'Please enter a username.',
+            'min_length': 'Please enter a username greater than 2.',
+        },
+    )
 
