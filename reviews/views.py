122c64141d4abeebe8eff0a602ad1cef449b2f0f
Qu <QKL@blackcapitaltechnology.com>
2026-07-29 23:13:11 +0200

    Enrich data for templateview

--- reviews/views.py
+++ reviews/views.py
@@ -29,3 +29,7 @@ class ReviewView(View):
 
 class ThankYouView(TemplateView):
    template_name = "reviews/thank_you.html"
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['message'] = "Thank you for your review!"
+       return context
